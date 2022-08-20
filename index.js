bf44effc29295c837b4bd2895a8e1f2a4ba684e7
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-20 15:10:49 +0800

    Add boilerplate

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import connectDB from "./app/db/index.js";
+import ENV from "./app/env/index.js";
+
+const app = express();
+
+app.use(bodyParser.json(), bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+const start = () => {
+  connectDB(ENV.MONGO_CON).then(() => {
+    console.log(`Database connected to ${ENV.MONGO_CON}`);
+
+    app.listen(ENV.PORT, () => {
+      console.log(`Server started on port ${ENV.PORT}`);
+    });
+  });
+};
+
+start();
