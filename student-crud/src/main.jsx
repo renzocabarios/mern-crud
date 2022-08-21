0112585efdda6f2b48d1a8ed8baa92cd66fa8096
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-21 16:43:58 +0800

    Add routes to views

--- student-crud/src/main.jsx
+++ student-crud/src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Users from "./views/Users";
+import Add from "./views/Add";
+import Edit from "./views/Edit";
+import NavBar from "./components/NavBar";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <div className="body">
+      <NavBar />
+
+      <div className="views">
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="add" element={<Add />} />
+          <Route path="edit/:id" element={<Edit />} />
+        </Routes>
+      </div>
+    </div>
+  </BrowserRouter>
+);
